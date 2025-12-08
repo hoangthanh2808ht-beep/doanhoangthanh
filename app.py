8aa867db5694f676b6d469e73f14bd97931ef006
hoangthanh2808ht-beep <hoangthanh2808.ht@gmail.com>
2025-12-08 13:54:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -588,5 +588,6 @@ with tab_ban_do:
 
     # --- MẶC ĐỊNH KHI MỚI VÀO ---
     else:
-        m = folium.Map(location=[13.9785, 108.0051], zoom_start=14, tiles="cartodbpositron")
+        m = folium.Map(location=[13.9785, 108.0051], zoom_start=14, tiles="OpenStreetMap")
         st_folium(m, width=1200, height=600, returned_objects=[])
+
